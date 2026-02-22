63c7ab02d1c892ee4247fed32a534975a9fd0374
sukkibhat9887-code <sukkibhat9887@gmail.com>
2026-02-23 00:07:49 +0530

    Delete nested.py

--- nested.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter the number"))
-
-if num > 0:
-    if num % 2 == 0:
-        print("Positive Even")
-    else:
-        print("Positive Odd")
-else:
-    print("Negative Number")
